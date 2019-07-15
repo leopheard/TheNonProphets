da4a9565d74cd2172adda36f4ab1de2a0c9c0ec3
leopheard <leopheard.misc@gmail.com>
2019-07-14 20:22:09 -0400

    initial up

---
+++ README.md
@@ -0,0 +1,12 @@
+#The Non-Prophets<br>
+=============================
+
+KODI / XBMC / Firestick audio addon for the Non-Prophets podcasts<br>
+
+<a href="http://www.nonprophetsradio.com/">nonprophetsradio.com</a><br>
+
+The Non Prophets is a live internet radio show airing on on Tuesdays starting at 7:30 PM Central (19:00-20:30 UTC).<br>
+
+To install this addon, either use the Kodi Github installer addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access. Then on the Kodi, go to addons > install from zip file.
+
+<a href="https://www.nonprophetsradio.com/"><img src="http://www.nonprophetsradio.com/images/nonprophetsall.jpg">
---
+++ addon.py
@@ -0,0 +1,57 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import thenonprophets
+
+plugin = Plugin()
+
+URL = "https://www.spreaker.com/show/3254964/episodes/feed"
+
+@plugin.route('/')
+def main_menu():
+    """
+    main menu 
+    """
+    items = [
+        {
+            'label': plugin.get_string(30000), 
+            'path': plugin.url_for('all_episodes'),
+            'thumbnail': "/resources/media/icon.png"},
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "/resources/media/icon.png"},
+    ]
+
+    return items
+
+
+@plugin.route('/all_episodes/')
+def all_episodes():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = thenonprophets.get_soup(URL)
+    
+    playable_podcast = thenonprophets.get_playable_podcast(soup)
+    
+    items = thenonprophets.compile_playable_podcast(playable_podcast)
+
+    return items
+
+
+@plugin.route('/all_episodes1/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = thenonprophets.get_soup(URL)
+    
+    playable_podcast1 = thenonprophets.get_playable_podcast1(soup)
+    
+    items = thenonprophets.compile_playable_podcast1(playable_podcast1)
+
+    return items
+
+
+
+if __name__ == '__main__':
+    plugin.run()
